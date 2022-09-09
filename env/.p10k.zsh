50e4360d71b10ea1d1274ae68be3f0ac32e05c06
ChenSmallX <24445771+ChenSmallX@users.noreply.github.com>
2022-09-10 01:34:56 +0800

    [UPD] modify the p10k prompt elements

--- env/.p10k.zsh
+++ env/.p10k.zsh
@@ -34,11 +34,17 @@
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
     os_icon                 # os identifier
-    dir                     # current directory
-    vcs                     # git status
+    user
+    host
+    vcs
     # =========================[ Line #2 ]=========================
+    newline
+    dir_writable
+    dir                     # current directory
+    #vcs                     # git status
+    # =========================[ Line #3 ]=========================
     newline                 # \n
-    prompt_char             # prompt symbol
+    prompt_char           # prompt symbol
   )
 
   # The list of segments shown on the right. Fill it with less important segments.
@@ -94,7 +100,7 @@
     vim_shell               # vim shell indicator (:sh)
     midnight_commander      # midnight commander shell (https://midnight-commander.org/)
     nix_shell               # nix shell (https://nixos.org/nixos/nix-pills/developing-with-nix-shell.html)
-    # vi_mode               # vi mode (you don't need this if you've enabled prompt_char)
+    vi_mode                 # vi mode (you don't need this if you've enabled prompt_char)
     # vpn_ip                # virtual private network indicator
     # load                  # CPU load
     # disk_usage            # disk usage
@@ -103,15 +109,27 @@
     todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
     taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
+    date
     time                    # current time
     # =========================[ Line #2 ]=========================
     newline
+    load
+    disk_usage
+    ram
+    swap
     # ip                    # ip address and bandwidth usage for a specified network interface
     # public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
-    # battery               # internal battery
-    # wifi                  # wifi speed
+    battery               # internal battery
+    wifi                  # wifi speed
     # example               # example user-defined segment (see prompt_example function below)
+    # =========================[ Line #3 ]========================
+    newline
+    status
+    background_jobs
+    history
+    proxy
+    vpn_ip
   )
 
   # Defines character set used by powerlevel10k. It's best to let `p10k configure` set it for you.
