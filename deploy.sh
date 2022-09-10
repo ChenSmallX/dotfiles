2e506a43e014e38a78916a5f292470781f783c0a
ChenSmallX <641751205@qq.com>
2022-09-10 16:20:21 +0800

    [ADD] make the deploy step readable.

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ SOURCE_DIR="${SCRIPT_DIR}/env"
 TARGET_DIR="$(readlink -f ~)"
 
 echo "Start deploy the dotfiles..."
+echo ""
 
 for file in $(ls -a "${SOURCE_DIR}" ); do
     if [ "${file}" == "." ] || [ "${file}" == ".." ]; then
@@ -18,18 +19,24 @@ for file in $(ls -a "${SOURCE_DIR}" ); do
     dst="${TARGET_DIR}/${file}"
 
     if [ -e ${dst} ]; then
-        echo "${dst} is already exist, skip!"
+        echo "[$(basename ${dst})] is already exist, skip!"
+        sleep 0.2
         continue
     fi
 
     ln -s "${src}" "${dst}"
-    echo "Link the ${file} succeed!"
+    echo "Link the $(basename ${file}) succeed!"
+    sleep 0.2
 done
 
+echo ""
 echo "Congratulation! Deploy all the dotfiles succeed!"
 echo "Please restart the shell or execute \"source ~/.zshrc\""
+echo ""
 
-echo "Switching shell to zsh... It need you input your passcode maybe."
+echo "Switching the default SHELL to zsh now."
+echo "It may need you to input your passcode.(Enter to continue / Ctrl-C to cancel)"
+read
 chsh -s $(which zsh)
 
 echo ""
