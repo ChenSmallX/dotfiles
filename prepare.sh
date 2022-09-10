d3c99225b28c1d49c097ac69935a120c8ab87afc
ChenSmallX <641751205@qq.com>
2022-09-10 14:28:57 +0800

    [MDF] add softlink function

--- prepare.sh
+++ prepare.sh
@@ -14,13 +14,31 @@ P10K_SRC="${DEP_DIR}/powerlevel10k"
 P10K_DST="${ENV_DIR}/.oh-my-zsh/custom/themes/powerlevel10k"
 
 ZSH_PLUGIN_AUTO_SUGGESTIONS_SRC="${DEP_DIR}/zsh-autosuggestions"
-ZSH_PLUGIN_AUTO_SUGGESTIONS_DST="${ENV_DIR}/.oh-my-zsh/custom/plugin/zsh-autosuggestions"
+ZSH_PLUGIN_AUTO_SUGGESTIONS_DST="${ENV_DIR}/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
 
 ZSH_PLUGIN_COMPLETIONS_SRC="${DEP_DIR}/zsh-completions"
-ZSH_PLUGIN_COMPLETIONS_DST="${ENV_DIR}/.oh-my-zsh/custom/plugin/zsh-completions"
+ZSH_PLUGIN_COMPLETIONS_DST="${ENV_DIR}/.oh-my-zsh/custom/plugins/zsh-completions"
 
 ZSH_PLUGIN_SYNTAX_SRC="${DEP_DIR}/zsh-syntax-highlighting"
-ZSH_PLUGIN_SYNTAX_DST="${ENV_DIR}/.oh-my-zsh/custom/plugin/zsh-syntax-highlighting"
+ZSH_PLUGIN_SYNTAX_DST="${ENV_DIR}/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+
+
+# create a soft link if it's not exist
+# $1 src_path
+# $2 dst_path
+function softlink()
+{
+    src=$1
+    dst=$2
+
+    mkdir -p $(dirname ${dst})
+    if [ -e "${dst}" ]; then
+        echo "The ${dst} is already exist, skip."
+        return
+    fi
+    ln -s "${src}" "${dst}"
+    echo "prepare $(basename ${dst}) succeed!"
+}
 
 # install required tools
 sudo apt install -y zsh
@@ -34,17 +52,14 @@ echo ' Done!'
 
 # combine the dependencies
 echo "put oh-my-zsh"
-mkdir -p $(dirname ${OH_MY_ZSH_DST})
-ln -s "${OH_MY_ZSH_SRC}" "${OH_MY_ZSH_DST}"
+softlink "${OH_MY_ZSH_SRC}" "${OH_MY_ZSH_DST}"
 
 echo "put powerlevel10k"
-mkdir -p $(dirname ${P10K_DST})
-ln -s "${P10K_SRC}" "${P10K_DST}"
+softlink "${P10K_SRC}" "${P10K_DST}"
 
 echo "put zsh plugins"
-mkdir -p $(dirname ${ZSH_PLUGIN_AUTO_SUGGESTIONS_DST})
-ln -s "${ZSH_PLUGIN_AUTO_SUGGESTIONS_SRC}" "${ZSH_PLUGIN_AUTO_SUGGESTIONS_DST}"
-ln -s "${ZSH_PLUGIN_COMPLETIONS_SRC}" "${ZSH_PLUGIN_COMPLETIONS_DST}"
-ln -s "${ZSH_PLUGIN_SYNTAX_SRC}" "${ZSH_PLUGIN_SYNTAX_DST}"
+softlink "${ZSH_PLUGIN_AUTO_SUGGESTIONS_SRC}" "${ZSH_PLUGIN_AUTO_SUGGESTIONS_DST}"
+softlink "${ZSH_PLUGIN_COMPLETIONS_SRC}" "${ZSH_PLUGIN_COMPLETIONS_DST}"
+softlink "${ZSH_PLUGIN_SYNTAX_SRC}" "${ZSH_PLUGIN_SYNTAX_DST}"
 
 chsh -s $(which zsh)
