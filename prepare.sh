080a82fd99c46b451e07b0c63ac5f0b264bc4084
ChenSmallX <641751205@qq.com>
2022-09-10 14:03:11 +0800

    [ADD] add zsh plugins

--- prepare.sh
+++ prepare.sh
@@ -13,6 +13,15 @@ OH_MY_ZSH_DST="${ENV_DIR}/.oh-my-zsh"
 P10K_SRC="${DEP_DIR}/powerlevel10k"
 P10K_DST="${ENV_DIR}/.oh-my-zsh/custom/themes/powerlevel10k"
 
+ZSH_PLUGIN_AUTO_SUGGESTIONS_SRC="${DEP_DIR}/zsh-autosuggestions"
+ZSH_PLUGIN_AUTO_SUGGESTIONS_DST="${ENV_DIR}/.oh-my-zsh/custom/plugin/zsh-autosuggestions"
+
+ZSH_PLUGIN_COMPLETIONS_SRC="${DEP_DIR}/zsh-completions"
+ZSH_PLUGIN_COMPLETIONS_DST="${ENV_DIR}/.oh-my-zsh/custom/plugin/zsh-completions"
+
+ZSH_PLUGIN_SYNTAX_SRC="${DEP_DIR}/zsh-syntax-highlighting"
+ZSH_PLUGIN_SYNTAX_DST="${ENV_DIR}/.oh-my-zsh/custom/plugin/zsh-syntax-highlighting"
+
 # install required tools
 sudo apt install -y zsh
 sudo apt install -y vim curl git
@@ -31,5 +40,11 @@ ln -s "${OH_MY_ZSH_SRC}" "${OH_MY_ZSH_DST}"
 echo "put powerlevel10k"
 mkdir -p $(dirname ${P10K_DST})
 ln -s "${P10K_SRC}" "${P10K_DST}"
+
+echo "put zsh plugins"
+mkdir -p $(dirname ${ZSH_PLUGIN_AUTO_SUGGESTIONS_DST})
+ln -s "${ZSH_PLUGIN_AUTO_SUGGESTIONS_SRC}" "${ZSH_PLUGIN_AUTO_SUGGESTIONS_DST}"
+ln -s "${ZSH_PLUGIN_COMPLETIONS_SRC}" "${ZSH_PLUGIN_COMPLETIONS_DST}"
+ln -s "${ZSH_PLUGIN_SYNTAX_SRC}" "${ZSH_PLUGIN_SYNTAX_DST}"
 
 chsh -s $(which zsh)
